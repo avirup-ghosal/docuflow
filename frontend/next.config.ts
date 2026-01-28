7f121b7f8387a556893f199003a7be5c6f2da687
Avirup Ghosal <avirupghosal2023@gmail.com>
2026-01-29 01:12:35 +0530

    updated next config for redirects

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        // redirects /api/auth/login -> http://127.0.0.1:80/auth/login
+        destination: 'http://127.0.0.1:80/:path*', 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
